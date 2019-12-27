e55ccc735b1aa2ae8725732bba65202819c3de43
Paweł Sworobowicz <sworobowicz.pawel@gmail.com>
2019-12-27 16:18:28 +0100

    Add files via upload

---
+++ precache-manifest.1ea30f52008cbaa754ce37b2f04afc20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083c4dbbe20b76ca19a75ed1d8efbb47",
+    "url": "/mersive/index.html"
+  },
+  {
+    "revision": "43546454e6f0727893bb",
+    "url": "/mersive/static/css/main.1d10729b.chunk.css"
+  },
+  {
+    "revision": "38c8dca527cc75bc6b86",
+    "url": "/mersive/static/js/2.0376d261.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/mersive/static/js/2.0376d261.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43546454e6f0727893bb",
+    "url": "/mersive/static/js/main.01239b72.chunk.js"
+  },
+  {
+    "revision": "15320ec61209938f5b82",
+    "url": "/mersive/static/js/runtime-main.d686fd88.js"
+  }
+]);
